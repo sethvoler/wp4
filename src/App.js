dd392496c4871725cdde14151ffa6f0c974f26cb
sethvoler <353619470@qq.com>
2019-05-15 20:45:05 +0800

    change layout

--- src/App.js
+++ src/App.js
@@ -8,17 +8,50 @@ export default class App extends React.Component {
       name: ''
     }
   }
+  changeName () {
+    test(3).then(res => this.setState({name: res.data}))
+  }
   componentDidMount () {
-    test(5).then(res => this.setState({name: res.data}))
+    this.changeName()
   }
   render () {
     return (
-      <div>
+      <div style={{
+        marginTop: '20px',
+        textAlign: 'center',
+        fontSize: '32px',
+        color: 'red'
+      }}>
         {
           this.state.name === 'Not Find'
-          ? '我是无名氏'
-          : `My name is ${this.state.name}`
+          ? <p>我是无名氏</p>
+          : <p>My name is {this.state.name}</p>
         }
+        <div style={{
+          position: 'absolute',
+          top: '200px',
+          left: 0,
+          width: '100%',
+          fontSize: '18px',
+        }}>
+          <p style={{
+            fontSize: '16px',
+            color: '#ccc'
+          }}>Tips: 点击按钮提问</p>
+          <button style={{
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            paddingTop: '10px',
+            paddingBottom: '10px',
+            fontSize: '18px',
+            backgroundColor: '#5678ff',
+            borderRadius: '6px',
+            outline: 'none',
+            border: 0,
+            cursor: 'pointer'
+          }}
+          onClick={() => this.changeName()}>What's your name?</button>
+        </div>
       </div>
     )
   }
